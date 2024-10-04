9552bc241a8d90840ba7b31dde9c971ba83d6125
brilinho <jgsaraiva2004@gmail.com>
2024-10-04 16:12:13 -0300

    Finalização de rotas e implementação da validação de usuário

--- src/services/AutenticacaoService.js
+++ src/services/AutenticacaoService.js
@@ -15,5 +15,25 @@ module.exports = {
                 }
             )
         })
+    },
+
+    verificarConta: (email_professor, senha_professor) => {
+        return new Promise((resolve, reject) => {
+            database.query(
+                `SELECT * FROM autenticacao WHERE email_professor = ? AND senha_professor = ?`,
+                [email_professor, senha_professor],
+                (err, result) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    if (result.length > 0) {
+                        resolve(result[0]); // Retorna o primeiro resultado encontrado
+                    } else {
+                        resolve(null); // Nenhuma conta encontrada
+                    }
+                }
+            );
+        });
     }
 }
